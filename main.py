807b2f291511f439b0571b13c80d42c30f3c7fc7
Mehrshad Firouzian <mrfirouz007@gmail.com>
2023-10-18 02:19:03 +0330

    add menu/open functionality

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog as fd
 
+from pathlib import Path
+
 
 class TextEditor:
     def __init__(self, root=None, filename=None):
@@ -9,21 +11,45 @@ class TextEditor:
 
         self.root = root
 
-        self.create_entry()
-        self.create_menu()
+        self.__create_entry()
+        self.__create_menu()
 
-    def create_entry(self) -> None:
+    def __create_entry(self) -> None:
         self.text_widget = tk.Text(self.root)
         self.text_widget.pack(fill='both', expand=True)
 
-    def create_menu(self) -> None:
+    def clear(self) -> None:
+        """
+        clears text_widget
+        """
+        self.text_widget.delete('1.0', 'end')
+
+    def openfile(self, filename: str) -> None:
+        """ 
+        Reads a file into text_widget
+
+        Args:
+            filename (str): file name to be read
+        """
+        self.clear()
+        with open(filename, 'r') as f:
+            try:
+                for d, line in enumerate(f.readlines(), start=1):
+                    self.text_widget.insert(f'{d}.0', line)
+            except UnicodeDecodeError as e:
+                self.text_widget.insert('end', str(e))
+
+    def askopenfile(self) -> None:
+        filename = fd.askopenfilename()
+        self.openfile(filename)
+
+    def __create_menu(self) -> None:
         menubar = tk.Menu(self.root)
         self.root.config(menu=menubar)
 
         file_menu = tk.Menu(menubar, tearoff=False)
         file_menu.add_command(label='New', command=TextEditor)
-        file_menu.add_command(label='Open...', command=fd.askopenfile)
-
+        file_menu.add_command(label='Open...', command=self.askopenfile)
         file_menu.add_command(label='Close', command=self.root.destroy)
 
         menubar.add_cascade(label='File', menu=file_menu)
